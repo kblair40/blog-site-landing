dd06a91b75f3e8082e32d70614100f5249039a8d
Kevin Blair <kblair40@gmail.com>
2022-09-19 08:06:58 -0500

    Add custom _document to handle font loading

---
+++ pages/_document.js
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Bitter:wght@100;200&family=Roboto+Slab:wght@100;200&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
